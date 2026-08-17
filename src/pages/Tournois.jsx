21636b1172e26adb1a0fec736f279f4290a36e17
domin <0balz.17peluche27@gmail.com>
2026-08-17 19:54:57 +0200

    bb

--- src/pages/Tournois.jsx
+++ src/pages/Tournois.jsx
@@ -1,4 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react'
+import { supabase } from '../supabaseClient'
+
+const DEPARTEMENTS = [ 
+
+  {id: 'dep1' , nom : 'Département 1', image: '/image/' , lienOfficiel : ""}
+]
+
 
 function Liens() {
   const [titre, setTitre] = useState("");
@@ -23,12 +30,13 @@ function Liens() {
   };
 
   return (
-    <div>
-      <h2>Ajouter un lien</h2>
+    <div className= "bg-placeholders   ">
+      <h2 className="mx-4"> <strong> Ajouter un lien</strong></h2>
 
       <form onSubmit={ajouterLien}>
 
         <input
+          className = "mx-4  bg-input text-white placeholder-white border border-white rounded px-3 py-2"
           type="text"
           placeholder="Titre du lien"
           value={titre}
@@ -36,13 +44,17 @@ function Liens() {
         />
 
         <input
+          className = "mx-4 bg-input text-white placeholder-white border border-white rounded px-3 py-2"
           type="url"
           placeholder="https://exemple.com"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
         />
 
-        <button type="submit">
+       <button
+          type="submit"
+          className="border border-white bg-[#073b14] text-white px-4 py-2 rounded"
+        >
           Ajouter
         </button>
 
@@ -68,12 +80,17 @@ function Liens() {
 
 export default function Tournois() {
   return (
-    <div>
-      <h1>Tournois</h1>
+    <div className="bg-black min-h-screen">
+      <div className="admin-page max-w-4xl mx-auto p-4">
+        <div className="relative overflow-hidden rounded-lg mb-6">
+          <div className="absolute inset-0 motif-damier2"></div>
+          <h1 className="z-10 relative text-center text-3xl font-bold text-heading  p-4"><mark className  = "StitreB">Tournois </mark></h1>
+          </div>
 
-      <p>Contenu de la page Tournois</p>
+        <h2 className=""> <strong> Les liens par région</strong></h2>
 
-      <Liens />
+        <Liens />
+      </div>
     </div>
   );
 }
